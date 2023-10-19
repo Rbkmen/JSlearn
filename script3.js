f7d404640669e7b48af199a5aee964b0b445b0b6
Andrei <rbkmen23@gmail.com>
2023-10-19 20:24:32 +0300

    Gogo

--- script3.js
+++ script3.js
@@ -21,20 +21,20 @@
 
 // Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+const numberOfFilms = prompt("Сколько фильмов вы посмотрели?", "");
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+	b = prompt("На сколько оцените его?", ""),
+	c = prompt("Один из последних просмотренных фильмов?", ""),
+	d = prompt("На сколько оцените его?", "");
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
