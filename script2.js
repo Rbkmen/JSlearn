196577e16e3b5ddae5eb66bf65dd8200623920b6
Andrei <rbkmen23@gmail.com>
2023-10-19 20:11:50 +0300

    add script3

--- script2.js
+++ script2.js
@@ -13,4 +13,4 @@ answers[0] = prompt("Как ваше имя?", "");
 answers[1] = prompt("Какая ваша фамилия?", "");
 answers[2] = prompt("Сколько вам лет?", "");
 
-console.log(typeof(answers));
+console.log(typeof(answers)); 
